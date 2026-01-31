a44cb9a53e66f95056e249f9522f899529dc251c
Devkarpenter <deveshkarpenter1998@gmail.com>
2026-01-31 15:02:17 +0530

    fixing white space in mobile view

--- src/App.jsx
+++ src/App.jsx
@@ -75,7 +75,7 @@ function App() {
   return (
     <Router>
       <ScrollToTop />
-      <div className="flex flex-col min-h-[100svh]  from-slate-950 via-slate-900 to-slate-950">
+      <div className="flex flex-col min-h-[100svh]">
 
         {/* Background glow */}
         <div className="fixed inset-0 -z-10 pointer-events-none">
